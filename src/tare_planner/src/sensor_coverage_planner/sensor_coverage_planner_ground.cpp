b2dd07990196a186af9b3a1ad3ff96b474f39d8d
Chao Cao <caochao39@gmail.com>
2021-07-27 19:31:43 -0400

    update local momentum

--- src/tare_planner/src/sensor_coverage_planner/sensor_coverage_planner_ground.cpp
+++ src/tare_planner/src/sensor_coverage_planner/sensor_coverage_planner_ground.cpp
@@ -952,8 +952,24 @@ bool SensorCoveragePlanner3D::GetLookAheadPoint(const exploration_path_ns::Explo
     exit(1);
   }
 
-  double dx = pd_.lookahead_point_direction_.x();
-  double dy = pd_.lookahead_point_direction_.y();
+  // double dx = pd_.lookahead_point_direction_.x();
+  // double dy = pd_.lookahead_point_direction_.y();
+
+  double lx = 1.0;
+  double ly = 0.0;
+  double dx = 1.0;
+  double dy = 0.0;
+  if (pd_.moving_forward_)
+  {
+    lx = 1.0;
+  }
+  else
+  {
+    lx = -1.0;
+  }
+
+  dx = cos(pd_.robot_yaw_) * lx - sin(pd_.robot_yaw_) * ly;
+  dy = sin(pd_.robot_yaw_) * lx + cos(pd_.robot_yaw_) * ly;
 
   double forward_angle_score = -2;
   double backward_angle_score = -2;
